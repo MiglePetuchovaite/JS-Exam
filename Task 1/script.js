56dfcce1a1c5186f3c8b8eef1c10e9fb2aeb31a4
Migle Petuchovaite <email@email.comMigle>
2022-10-09 15:40:09 +0300

    Task 1

--- Task 1/script.js
+++ Task 1/script.js
@@ -9,5 +9,48 @@ Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
 
+function convertToLb(kgValue) {
+  return kgValue * 2.2046;
+}
 
+function convertToG(kgValue) {
+  return kgValue * 0.001;
+}
 
+function convertToOz(kgValue) {
+  return kgValue * 35.274;
+}
+
+const submitBtn = document.getElementById("submit-btn");
+
+submitBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const outputDiv = document.getElementById("output");
+  outputDiv.innerHTML = "";
+  outputDiv.classList.add("d-none");
+
+  const kgValue = document.getElementById("search").value;
+
+  if (kgValue < 1) {
+    return;
+  }
+
+  outputDiv.classList.remove("d-none");
+
+  const lbValue = convertToLb(kgValue);
+  const gValue = convertToG(kgValue);
+  const ozValue = convertToOz(kgValue);
+
+  const lbDiv = document.createElement("div");
+  lbDiv.appendChild(document.createTextNode("lb: " + lbValue));
+
+  const gDiv = document.createElement("div");
+  gDiv.appendChild(document.createTextNode("g: " + gValue));
+
+  const ozDiv = document.createElement("div");
+  ozDiv.appendChild(document.createTextNode("oz: " + ozValue));
+
+  outputDiv.appendChild(lbDiv);
+  outputDiv.appendChild(gDiv);
+  outputDiv.appendChild(ozDiv);
+});
