1b831ea7b18756aa43a354efc0c38fb3366e1dc1
Migle Petuchovaite <email@email.comMigle>
2022-10-09 16:46:32 +0300

    Task 4

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,4 +9,41 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "cars.json";
+const fetchData = async (endpoint) => {
+  try {
+    const response = await fetch(endpoint);
+    if (response.ok) {
+      return await response.json();
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const buildCard = (data) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const brand = document.createElement("h2");
+  brand.innerText = "Brand: " + data.brand;
+
+  card.appendChild(brand);
+
+  data.models.forEach((element) => {
+    const model = document.createElement("p");
+    model.innerText = element;
+
+    card.appendChild(model);
+  });
+
+  return card;
+};
+
+const output = document.getElementById("output");
+
+fetchData(ENDPOINT).then((brands) => {
+  brands.forEach((element) => {
+    output.appendChild(buildCard(element));
+  });
+});
